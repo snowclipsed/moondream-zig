6c469e34de93d76e290b283180ce8c06a73157c0
snowclipsed <harrybishnoi@gmail.com>
2024-12-14 16:35:02 -0500

    added layernorm to text_model

--- src/text_model.zig
+++ src/text_model.zig
@@ -1,8 +1,10 @@
 const std = @import("std");
+const print = std.debug.print;
 const Allocator = std.mem.Allocator;
 const Weights = @import("weights.zig").Weights;
 const Config = @import("config.zig").Config;
 const Tensor = @import("tensor.zig").Tensor;
+const ops = @import("ops.zig");
 
 pub const TextModel = struct {
     const Self = @This();
@@ -28,11 +30,37 @@ pub const TextModel = struct {
     // TODO: Implement deinit
     pub fn text_decoder(self: Self, input_embeds: Tensor(f32)) !void {
         const dim = self.config.dim;
+        const eps = 1e-5; // TODO move to config
         var hidden_BTC = try input_embeds.copy();
 
-        std.debug.print("hidden_BTC shape: {d}\n", .{hidden_BTC.shape});
-        std.debug.print("dim: {d}\n", .{dim});
+        print("hidden_BTC shape: {d}\n", .{hidden_BTC.shape});
+        print("dim: {d}\n", .{dim});
+
+        for (0..self.config.n_layers) |layer| {
+            print("Layer: {d}\n", .{layer});
+
+            var layer_ln_w = try self.weights.t_ln_w.getDimensionSlice(0, layer);
+            var layer_ln_b = try self.weights.t_ln_b.getDimensionSlice(0, layer);
+            var ln_in = try ops.layerNorm(
+                f32,
+                hidden_BTC,
+                layer_ln_w,
+                layer_ln_b,
+                eps,
+            );
+
+            // frees
+            defer layer_ln_w.deinit();
+            defer layer_ln_b.deinit();
+            ln_in.deinit();
+        }
 
         defer hidden_BTC.deinit();
     }
+
+    // fn attention_block(self: Self) !Tensor(f32) {
+    //     const dim = self.config.dim;
+    //     const n_heads = self.config.n_heads;
+    //     const head_dim = self.config.head_dim;
+    // }
 };
